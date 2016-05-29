6c5fc84498dee57be3ae455e0f19dc705726d19f
LuisAlfaroR <lalfaror16@gmail.com>
2016-05-28 23:05:07 -0600

    ok

--- Modulos/Proyecto_final.v
+++ Modulos/Proyecto_final.v
@@ -3,8 +3,9 @@
 module Proyecto_final(
     input clk,
     input reset,
-    input aumenta,disminuye,siguiente,anterior,
-    output [7:0] ano,mes,dia,hora,min,seg,ht,mt,st
+    input aumenta,disminuye,siguiente,anterior,Listo_es,
+    output [7:0] ano,mes,dia,hora,min,seg,ht,mt,st,
+	 output Listo_ht
     );
 
 wire [9:0] address;
@@ -22,7 +23,7 @@ Registro_Teclado Teclado (
     .anterior(anterior), 
     .Port_ID(port_id), 
     .In_Port(in_port)
-    );
+    );	
 
 kcpsm3 PicoBlaze (
     .address(address), 
@@ -43,6 +44,35 @@ prueba ROM (
     .instruction(instruction), 
     .clk(clk)
     );
-	 
+
+Resgistro_a_desde_RTC Registro_a_desde_RTC (
+    .clk(clk), 
+    .reset(reset), 
+    .write(write_strobe), 
+    .Listo_es(Listo_es), 
+    .Out_Port(out_port), 
+    .Port_ID(port_id), 
+    .In_Port(In_Port), 
+    .ano(ano), 
+    .mes(mes), 
+    .dia(dia), 
+    .horas(horas), 
+    .minutos(minutos), 
+    .segundos(segundos), 
+    .ht(ht), 
+    .mt(mt), 
+    .st(st), 
+    .anole(8'h2), 
+    .mesle(8'h2), 
+    .diale(8'h2), 
+    .horasle(8'h2), 
+    .minutosle(8'h2), 
+    .segundosle(8'h2), 
+    .htle(8'h2), 
+    .mtle(8'h2), 
+    .stle(8'h2), 
+    .Listo_ht(Listo_ht), 
+    .Listo_esc(Listo_esc)
+    );	 
 
 endmodule
